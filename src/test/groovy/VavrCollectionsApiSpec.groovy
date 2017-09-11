3c43da6ff0b7c4e41bbfbc4037eb2331a6759c87
Rafal Gorzkowski <rafal.gorzkowski@stepwise.pl>
2017-09-11 11:56:04 +0200

    Remove last element from list using groovy way

--- src/test/groovy/VavrCollectionsApiSpec.groovy
+++ src/test/groovy/VavrCollectionsApiSpec.groovy
@@ -2,15 +2,13 @@ import io.vavr.API
 import io.vavr.collection.List
 import spock.lang.Specification
 
-import java.util.function.Predicate
-
 class VavrCollectionsApiSpec extends Specification {
 
     def "should test vavr list"() {
         given:
             def list = List.of(1, 2, 3, 1)
         when:
-            def modifiedList = list.removeLast(Predicate.isEqual(1)) // java way
+            def modifiedList = list.removeLast { it -> 1.equals(it) } // groovy way
         then:
             list == API.List(1, 2, 3, 1)
             modifiedList == API.List(1, 2, 3)
